d11a4f34c53dc5794041a90cb64457463c507d91
mauro1508 <mauro1508@gmail.com>
2014-12-09 12:28:50 -0200

    terceiro commit

--- TesteGit.java
+++ TesteGit.java
@@ -1,7 +1,8 @@
 public class TesteGit {
 
 	public static void main (String [] args) {
-
+		
+		//mais um cometario - 3 commit
 		System.out.println("Exemplo de teste de uso do git");
 		// teste - alterando o arquivo
 
